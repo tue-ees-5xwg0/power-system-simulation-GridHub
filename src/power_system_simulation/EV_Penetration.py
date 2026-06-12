869c85c146cbf65db56d5cee42ff21871408cde7
Julian <j.k.v.d.hurk@student.tue.nl>
2026-06-12 16:05:49 +0200

    Creation of EVP files

---
+++ src/power_system_simulation/EV_Penetration.py
@@ -0,0 +1,6 @@
+"""
+    This module contains the implementation of the EV penetration level function
+    as described in Assignment 3.
+    The module randomly assigns EV charging profiles to houses and runs a time-series
+    power flow analysis, returning two aggregation tables.
+"""
